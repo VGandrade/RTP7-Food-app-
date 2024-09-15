f7684a775ade38103e8b9347d93e1ae63874ab1c
rachelklee <iamrachelklee@gmail.com>
2024-09-15 17:55:19 -0400

    Set up restaurants database and restaurants page

--- Poject1/ourSite/FoodApp/views.py
+++ Poject1/ourSite/FoodApp/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import Restaurant
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world.")
+
+def restaurant_list(request):
+    # Query all restaurant objects
+    restaurants = Restaurant.objects.all()
+
+    # Pass the data to the template
+    return render(request, 'restaurants.html', {'restaurants': restaurants})
