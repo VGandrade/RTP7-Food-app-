84cd45041b1ec01ccf15134be12248c62d3b2594
Ifaz <ifaz.howl@gmail.com>
2024-09-22 00:27:03 -0400

    Searches Restaurants in the area with marker hopefully. Still working on functionality.

--- Poject1/ourSite/FoodApp/models.py
+++ Poject1/ourSite/FoodApp/models.py
@@ -11,11 +11,11 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=100, default="Unknown Name")
     location = models.CharField(max_length=255, default="Unknown Address")  # Address of the restaurant
-    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)  # Rating between 0.00 and 10.00
-    cuisine = models.CharField(max_length=100, default="Unknown Cuisine")
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)  # Rating between 0.00 and 5.00
 
     def __str__(self):
-        return f"{self.name} restaurant serving {self.cuisine} cuisine at {self.location} with rating {self.rating}"
+        return self.name
+
 
 class User(models.Model):
     class User(models.Model):
